e79b03e45fc41c1e8dd000848f91b1b465741696
Yang Honggang <yanghonggang@cmss.chinamobile.com>
2023-08-20 11:36:16 +0800

    去掉不需要的 redis 相关内容

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
 //! A simple key-value storage.
 pub mod config;
 mod error;
-mod resp;
 mod segment;
-mod server;
 mod store;
 pub mod util;
 
 pub use error::{Result, TinkvError};
-pub use server::Server;
 pub use store::{OpenOptions, Store};
